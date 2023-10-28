46e5ec4fd601a52954364be6ff42a673d6cbfeb0
Muntasir Ahmed <muntasirahmed2179@gmail.com>
2023-10-29 01:04:42 +0600

    audio file saved in media folder

--- bangla_accent_detection/dashboard/urls.py
+++ bangla_accent_detection/dashboard/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('recording-page/', views.recording_page, name='recording-page'),
     path('make-prediction/', views.make_prediction, name='make-prediction'),
 ]
--- bangla_accent_detection/dashboard/views.py
+++ bangla_accent_detection/dashboard/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import Document
+
+# dependencies for trained model
+import librosa
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
-def make_prediction(request):
+def recording_page(request):
     return render(request, 'main.html')
+
+def make_prediction(request):
+    if request.method == "POST":
+        audio = request.FILES['recording_file']
+        Document.objects.create(name=audio.name, file=audio)   # saving an audio file by creating an Document object
+    return render(request, 'result.html')
