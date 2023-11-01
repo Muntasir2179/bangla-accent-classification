fe9620e2f9be4745783f93cfe6b2d2f9e2a35e41
Muntasir Ahmed <muntasirahmed2179@gmail.com>
2023-11-01 12:42:18 +0600

    prediction chart and empty request handled

--- bangla_accent_detection/dashboard/views.py
+++ bangla_accent_detection/dashboard/views.py
@@ -24,25 +24,61 @@ def recording_page(request):
 
 def make_prediction(request):
     if request.method == "POST":
+        if not 'recording_file' in request.FILES:
+            text = "There is no audio input."
+            prediction_probabilities = np.zeros(13)
+
         # getting the audio file
         audio = request.FILES['recording_file']
         audio_file = Document.objects.create(name=audio.name, file=audio)   # saving an audio file by creating an Document object
         audio_path = audio_file.file.path
 
-        # extracting features from the enhanced audio file
-        features = features_extractor(audio_path).reshape(-1, 128)
-        
-        # loading the trained model
-        model = tf.keras.models.load_model('models/ANN_Model_val_acc_90%.h5')
-        result = model.predict(features.reshape(-1, 128), verbose=0)
-
-        # converting the result into onehot representation
-        one_hot_output = np.zeros(13)
-        one_hot_output[np.argmax(result)] = 1.0
-
+        # loading the onehot encoder
         with open('models/onehot_encoder_accent_classification.pkl', 'rb') as f:
-            encoder = pickle.load(f)
-        
-        print(encoder.inverse_transform(one_hot_output.reshape(1, -1)).squeeze())
+                encoder = pickle.load(f)
 
-    return render(request, 'result.html')
+        try:
+            title_text = "Prediction probabilities for each accent."
+
+            # extracting features from the enhanced audio file
+            features = features_extractor(audio_path).reshape(-1, 128)
+
+            # loading the trained model and making prediction on users input voice
+            model = tf.keras.models.load_model('models/ANN_Model_val_acc_90%.h5')
+            prediction_probabilities = model.predict(features.reshape(-1, 128), verbose=0)
+            prediction_probabilities = prediction_probabilities.squeeze()  # removing single dimension
+
+            # converting the result into onehot representation
+            one_hot_output = np.zeros(len(prediction_probabilities))
+            one_hot_output[np.argmax(prediction_probabilities)] = 1.0
+            
+            # getting the predicted class name
+            predicted_class = encoder.inverse_transform(one_hot_output.reshape(1, -1)).squeeze()
+        except Exception as e:
+            title_text = "There is no audio input."
+            prediction_probabilities = np.zeros(13)
+            predicted_class = None
+
+        print()
+        print(predicted_class)
+        print()
+
+        # getting all classes names from the encoder
+        all_class_names = [class_name.replace('x0_', '') for class_name in  encoder.get_feature_names_out()]
+
+        # creating dictionary of class and their corresponding probability score
+        probability_dict = dict(zip(all_class_names, prediction_probabilities))
+        for key, value in probability_dict.items():
+            print(f"{key} : {value}")
+        print()
+
+        context = {
+            'text': title_text,
+            'file_name': audio_path,
+            'labels': [key for key, _ in probability_dict.items()],
+            'probability': [value for _, value in probability_dict.items()],
+            'probability_dict': probability_dict,
+            'predicted_class': predicted_class
+        }
+
+    return render(request, 'result.html', context=context)
